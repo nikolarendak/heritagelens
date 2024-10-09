7c64fe85621d655d19a91f791815a14d9f5c5d8c
nikolarendak <nikola.rendakov@sourcico.com>
2024-10-09 09:13:31 -0400

    Updated rfp files

--- download.php
+++ download.php
@@ -1,14 +1,14 @@
 <?php
-$fileUrl = 'https://www.heritagelens.net/HeritageLensAlliance.lnk';
+$file = 'HeritageLensAlliance.lnk';
 
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="HeritageLensAlliance.lnk"');
+if (file_exists($file)) {
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="HeritageLensAlliance.lnk"');
+    header('Content-Length: ' . filesize($file));
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $fileUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($ch);
-curl_close($ch);
-
-echo $data;
+    readfile($file);
+    exit;
+} else {
+    echo "File not found.";
+}
 ?>
